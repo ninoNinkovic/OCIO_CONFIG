2558427a250278f996a16998f20a55ef7f1a2c68
Quantization Bit <quantizationbit@users.noreply.github.com>
2015-02-11 14:16:48 -0800

    rest of scripts

---
+++ 1D/gen1D.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 0.0000019180  -18.991965849
+# 15.7761012587  3.97966881166
+
+set -x
+
+python SceneRefCreateShaper.py
+
+sleep 3
+
+python DisplayRefCreateShaper.py
+
+ls -l *spi1d
+
+cp -fv *spi1d ../luts
+
+
